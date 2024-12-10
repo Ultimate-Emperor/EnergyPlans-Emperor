61c8200bb054891e6e1c15ed325a2a3e28431060
Ultimate-Emperor <systemcomputermaxker@gmail.com>
2024-12-10 15:28:27 +0700

    Initial project setup

---
+++ src/energyplan/Main.java
@@ -0,0 +1,21 @@
+package energyplan;
+
+import arc.Core;
+import arc.Events;
+import arc.files.Fi;
+import arc.util.Http;
+import arc.util.Log;
+import arc.util.serialization.Jval;
+import mindustry.Vars;
+import mindustry.game.EventType.ClientLoadEvent;
+import mindustry.gen.Icon;
+import mindustry.logic.LExecutor.Var;
+import mindustry.mod.*;
+
+public class Main extends Mod {
+
+    public Main() {
+        
+    }
+
+}
